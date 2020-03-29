53eaf3b00491d163b06e97b2944c0ce6c17fd4a5
Florin Pop <popflorin1705@yahoo.com>
2020-03-29 18:11:43 +0300

    added project to index

--- script.js
+++ script.js
@@ -43,6 +43,11 @@ const projects = [
 		name: 'insure-landing-page',
 		youtube:
 			'https://www.youtube.com/watch?v=9HVKR_hK0nY&list=PLgBH1CvjOA63Xvt0BaeQ7zL4KXX96Wbgp&index=10&t=0s'
+	},
+	{
+		name: 'pricing-toggle-component',
+		youtube:
+			'https://www.youtube.com/watch?v=NBkD-O7f4Bs&list=PLgBH1CvjOA63Xvt0BaeQ7zL4KXX96Wbgp&index=10&t=0s'
 	}
 ];
 
